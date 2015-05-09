658e31e28dadf1befd77d4c9086a676a22d68e30
KisaragiInaba <kimura@Inaba-MacBook-Air.local>
2015-05-09 14:24:47 +0900

    update

---
+++ mother/Assets/Kimura/Scripts/PanickTitle.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PanickTitle : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void OnButton1()
+	{
+		Application.LoadLevel("");
+	}
+
+	public void OnButton2()
+	{
+		Application.Quit ();
+	}
+}
